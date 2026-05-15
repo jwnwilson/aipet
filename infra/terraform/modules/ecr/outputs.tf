0cb76436572bf76b274200e8db3aa47022526023
Noel Wilson <noel@MacBookPro-3.powerhub>
2026-05-15 16:05:04 +0100

    feat(infra): add ECR Terraform module

---
+++ infra/terraform/modules/ecr/outputs.tf
@@ -0,0 +1,14 @@
+output "repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.this.repository_url
+}
+
+output "repository_arn" {
+  description = "ECR repository ARN"
+  value       = aws_ecr_repository.this.arn
+}
+
+output "ecr_push_policy_arn" {
+  description = "IAM policy ARN granting ECR push"
+  value       = aws_iam_policy.ecr_push.arn
+}
