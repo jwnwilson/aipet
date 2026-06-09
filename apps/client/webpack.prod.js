4ac60e704f8b31cc3f15bde18ab1fa45bbe0285f
Noel Wilson <noel@MacBookPro-3.powerhub>
2026-06-09 19:42:01 +0100

    feat: add content hash to prod bundle for browser cache-busting
    Browser cache headers on bundle.js were set to max-age=31536000,immutable,
    meaning browsers would serve a stale bundle for up to a year even after a
    CloudFront invalidation cleared the CDN cache.
    
    - Add html-webpack-plugin to inject the hashed script tag into index.html
    - Override output filename in webpack.prod.js: bundle.[contenthash:8].js
    - Remove hardcoded <script src="./js/bundle.js"> from index.html template;
      HtmlWebpackPlugin now injects the correct hashed path at build time
    - Exclude index.html from CopyPlugin since HtmlWebpackPlugin owns it
    
    Each new release produces a unique bundle filename, so browsers always
    fetch the latest version regardless of their local HTTP cache.

--- apps/client/webpack.prod.js
+++ apps/client/webpack.prod.js
@@ -5,6 +5,9 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
+    output: {
+        filename: "js/bundle.[contenthash:8].js",
+    },
     plugins: [
         new webpack.DefinePlugin({
             "process.env.VITE_SERVER_URL": JSON.stringify(process.env.VITE_SERVER_URL ?? ""),
